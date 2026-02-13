be178281c80bd840a566c33a1ad4949efedfc792
Dmitry <morozkind6@gmail.com>
2026-02-13 21:13:01 +0300

    review api-gateway

--- api-gateway/go.mod
+++ api-gateway/go.mod
@@ -4,6 +4,8 @@ go 1.25.6
 
 require (
 	github.com/spf13/viper v1.21.0
+	google.golang.org/grpc v1.78.0
+	google.golang.org/protobuf v1.36.11
 	shop/auth-service v0.0.0-00010101000000-000000000000
 )
 
@@ -22,8 +24,6 @@ require (
 	go.yaml.in/yaml/v3 v3.0.4 // indirect
 	golang.org/x/net v0.48.0 // indirect
 	golang.org/x/sys v0.40.0 // indirect
-	golang.org/x/text v0.33.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20251029180050-ab9386a59fda // indirect
-	google.golang.org/grpc v1.78.0 // indirect
-	google.golang.org/protobuf v1.36.11 // indirect
+	golang.org/x/text v0.34.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20260209200024-4cfbd4190f57 // indirect
 )
--- api-gateway/internal/handlers/auth/auth.go
+++ api-gateway/internal/handlers/auth/auth.go
@@ -1,19 +1,19 @@
 package authHandlers
 
 import (
-	"api-geteway/internal/auth"
 	"context"
 	"encoding/json"
 	"log/slog"
 	"net/http"
+	authproto "shop/auth-service/api/gen/go/api/proto"
 	"time"
 
 	"google.golang.org/protobuf/encoding/protojson"
 )
 
 type AuthHandler struct {
-	authClient *auth.Client
-	log        *slog.Logger
+	auth Auth
+	log  *slog.Logger
 }
 
 type registerReq struct {
@@ -27,20 +27,20 @@ type loginReq struct {
 	Password string `json:"password"`
 }
 
-func NewAuthHandler(c *auth.Client, l *slog.Logger) *AuthHandler {
+type Auth interface {
+	Register(ctx context.Context, email, password string, fullName string) (*authproto.RegisterResponse, error)
+	Login(ctx context.Context, email, password string) (*authproto.LoginResponse, error)
+	IsAdmin(ctx context.Context, userID int64) (bool, error)
+}
+
+func NewAuthHandler(a Auth, l *slog.Logger) *AuthHandler {
 	return &AuthHandler{
-		authClient: c,
-		log:        l,
+		auth: a,
+		log:  l,
 	}
 }
 
 func (h *AuthHandler) Register(w http.ResponseWriter, r *http.Request) {
-	// TODO:
-	// 1) json.Decoder -> registerReq
-	// 2) ctx, cancel := context.WithTimeout(r.Context(), 3*time.Second); defer cancel()
-	// 3) resp, err := h.client.Register(ctx, req.Email, req.Password, req.FullName)
-	// 4) если err -> h.log.Error(...); http.Error(...)
-	// 5) b, _ := protojson.Marshal(resp); w.Header().Set("Content-Type", "application/json"); w.Write(b)
 	const op = "Handlers.Auth.Register"
 
 	var rr registerReq
@@ -54,7 +54,7 @@ func (h *AuthHandler) Register(w http.ResponseWriter, r *http.Request) {
 	ctx, cancel := context.WithTimeout(r.Context(), 3*time.Second)
 	defer cancel()
 
-	resp, err := h.authClient.Register(ctx, rr.Email, rr.Password, rr.FullName)
+	resp, err := h.auth.Register(ctx, rr.Email, rr.Password, rr.FullName)
 	if err != nil {
 		h.log.Error("%s: %w", op, err)
 		http.Error(w, "internal server error", http.StatusInternalServerError)
@@ -69,7 +69,6 @@ func (h *AuthHandler) Register(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *AuthHandler) Login(w http.ResponseWriter, r *http.Request) {
-	// TODO: аналогично Register, но вызвать h.client.Login и вернуть LoginResponse
 	const op = "Handler.Auth.Login"
 
 	var lr loginReq
@@ -83,7 +82,7 @@ func (h *AuthHandler) Login(w http.ResponseWriter, r *http.Request) {
 	ctx, cancel := context.WithTimeout(r.Context(), 3*time.Second)
 	defer cancel()
 
-	resp, err := h.authClient.Login(ctx, lr.Email, lr.Password)
+	resp, err := h.auth.Login(ctx, lr.Email, lr.Password)
 	if err != nil {
 		h.log.Error("%s: %w", op, err)
 		http.Error(w, "internal server error", http.StatusInternalServerError)
